94e4a4a30ead3bf4a79026f2975cab8e6f0d7d6e
Diegocarrero <diegocar2413@gmail.com>
2023-04-19 16:34:10 -0400

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Home from '../pages/Home';
+import Index from '../pages/Index';
+import Show from '../pages/Show';
+import New from '../pages/New';
+import Edit from '../pages/Edit';
+import NotFound from '../pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/transactions' element={<Index />} />
+        <Route path='/transactions/:id' element={<Show />} />
+        <Route path='/transactions/new' element={<New />} />
+        <Route path='/transactions/:id/edit' element={<Edit />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }

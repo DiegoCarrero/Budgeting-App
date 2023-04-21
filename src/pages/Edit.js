0a5c3c7cd6c980116ac4bed98074c17fa9cea1a8
Diegocarrero <diegocar2413@gmail.com>
2023-04-21 17:43:02 -0400

    edit Edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,11 @@
-
+import EditTransaction from "../components/EditTransaction";
 
 
 export default function Edit() {
     return (
         <div>
-            
+            <h3>Edit Transaction</h3>
+            <EditTransaction />
         </div>
     )
 }

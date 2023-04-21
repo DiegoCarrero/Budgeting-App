88c14867db4d9d79b3871837e8bf09bc210d719d
Diegocarrero <diegocar2413@gmail.com>
2023-04-21 17:25:08 -0400

    edit New page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,11 @@
-
+import NewTransaction from "../components/NewTransaction";
 
 
 export default function New() {
     return (
         <div>
-            
+            <h3>Add New Transaction</h3>
+            <NewTransaction />
         </div>
     )
 }

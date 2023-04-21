f19910127a0f662e460c795f04cc8613454da976
Diegocarrero <diegocar2413@gmail.com>
2023-04-21 16:40:35 -0400

    edit TransactionDetails

--- src/components/TransactionDetails.js
+++ src/components/TransactionDetails.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import axios from "axios";
 
+const API = process.env.REACT_APP_API_URL;
 
 export default function TransactionDetails() {
+
+    const [transaction, setTransaction] = useState({});
+    let navigate = useNavigate();
+    let index = useParams();
+
+    useEffect(() => {
+        axios.get(`${API}/transactions/${index}`)
+        .then((response) => setTransaction(response.data.transactions))
+        .catch((error) => navigate("/not-found"));
+    }, [])
+
+    function deleteTransaction() {
+
+    }
+
     return (
-        <div>
-            
+        <div className="Transaction-Details">
+            <article>
+                <h3>{transaction.transaction}</h3>
+                <p>From: {transaction.from}</p>
+                <p>Date: {transaction.date}</p>
+                <p>Amount: {transaction.amount}</p>
+                <p>Category: {transaction.category}</p>
+            </article>
+
+            <Link to={`/transactions/${index}/edit`}>
+                <button>Edit</button>
+            </Link>
+
+            <button onClick={deleteTransaction} >Delete</button>
         </div>
     )
 }

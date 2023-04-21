1e3cd86b8ee4141d52e0f63009ac707da3389f58
Diegocarrero <diegocar2413@gmail.com>
2023-04-21 17:46:29 -0400

    edit notFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 
 export default function NotFound() {
     return (
         <div>
-            
+            <h1>Page not found. Please go back to <Link to='/'>homepage</Link>.</h1>
         </div>
     )
 }

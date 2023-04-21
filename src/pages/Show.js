688c41551da0ff0390c75118243ecbf4b4aa6580
Diegocarrero <diegocar2413@gmail.com>
2023-04-21 16:42:19 -0400

    edit show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,9 +1,10 @@
-
+import TransactionDetails from "../components/TransactionDetails"
 
 export default function Show() {
     return (
         <div>
-            
+            <h3>Transaction Details</h3>
+            <TransactionDetails />
         </div>
     )
 }
